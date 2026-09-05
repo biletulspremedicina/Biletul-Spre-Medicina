e9d40c93d26628b4f07c0f7f5bfec16df9629644
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-05 15:27:00 +0300

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -381,7 +381,7 @@ function TicketCard() {
                 Ia-ți biletul spre Medicină
                 <ArrowRight size={18} className="transition-transform duration-300 group-hover:translate-x-1" />
               </button>
-              <p className="text-[11px] text-stone-400">Fă-ti cont. Începe gratuit cu simulările de probă.</p>
+              <p className="text-[11px] text-stone-400">Fă-ți cont. Începe gratuit cu simulările de probă.</p>
             </div>
           </div>
         </div>
