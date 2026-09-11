fbb0cc08d2c9256833d9dedd4b0de6fbbd39d644
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-11 23:54:27 +0300

    Updated WhyChooseUs.tsx

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -117,7 +117,7 @@ export default function WhyChooseUs() {
               id="simulations"
               icon={
   <img 
-    src="public/Calendar.png" 
+    src="/Calendar.png" 
     alt="Iconita" 
     className="h-8 w-8 object-contain transition-transform duration-300 hover:scale-115 hover:-rotate-12 active:scale-95 cursor-pointer" 
   />
