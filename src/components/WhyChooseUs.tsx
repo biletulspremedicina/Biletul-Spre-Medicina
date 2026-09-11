80895820da5419908e3ed54372dc765b0d04abb3
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-11 23:54:41 +0300

    Updated WhyChooseUs.tsx

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -137,7 +137,7 @@ export default function WhyChooseUs() {
               id="exam"
               icon={
   <img 
-    src="public/Checkboard.png" 
+    src="/Checkboard.png" 
     alt="Iconita" 
     className="h-8 w-8 object-contain transition-transform duration-300 hover:scale-115 hover:-rotate-12 active:scale-95 cursor-pointer" 
   />
