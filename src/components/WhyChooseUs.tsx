a315da31d82b432edf996fab54c30f5e5140e4ca
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-11 23:07:59 +0300

    Updated WhyChooseUs.tsx

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -115,7 +115,13 @@ export default function WhyChooseUs() {
           <Reveal className="md:col-span-2 lg:col-span-7 motion-reduce:!translate-y-0 motion-reduce:!opacity-100 motion-reduce:!transition-none" delay={60}>
             <AdvantageCard
               id="simulations"
-              icon={<CalendarDays size={24} />}
+              icon={
+  <img 
+    src="/calea-catre-imaginea-ta.png" 
+    alt="Iconita" 
+    className="h-6 w-6 object-contain" 
+  />
+}
               title="Simulări zilnice"
               intro="Oferim acces la o gamă vastă și variată de simulări, concepute pentru a nu lăsa loc de surprindere la examen."
               benefits={BENEFIT_CHECKS}
