bc435c8608eec35f410ee7c40861793b520d69f0
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-11 23:27:15 +0300

    Updated WhyChooseUs.tsx

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -137,9 +137,9 @@ export default function WhyChooseUs() {
               id="exam"
               icon={
   <img 
-    src="public/Calendar.png" 
+    src="/calea-catre-imaginea-ta.png" 
     alt="Iconita" 
-    className="h-8 w-8 object-contain hover:animate-bounce cursor-pointer" 
+    className="h-8 w-8 object-contain transition-transform duration-300 hover:scale-115 hover:-rotate-12 active:scale-95 cursor-pointer" 
   />
 }
               title="Simulează cu adevărat experiența examenului"
