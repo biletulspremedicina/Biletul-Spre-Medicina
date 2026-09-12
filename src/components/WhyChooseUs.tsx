d8d6629078ceb71eb20c0ee0a6ac65af15055f36
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-12 17:02:56 +0300

    Updated WhyChooseUs.tsx

--- src/components/WhyChooseUs.tsx
+++ src/components/WhyChooseUs.tsx
@@ -204,7 +204,7 @@ export default function WhyChooseUs() {
           </p>
         </Reveal>
 
-        <div className="grid gap-5 lg:grid-cols-2 lg:items-start">
+       <div className="grid gap-5 lg:grid-cols-2 lg:items-start">
   <div className="contents lg:flex lg:flex-col lg:gap-5">
           <Reveal
             className="lg:col-span-7 motion-reduce:!translate-y-0 motion-reduce:!opacity-100 motion-reduce:!transition-none"
