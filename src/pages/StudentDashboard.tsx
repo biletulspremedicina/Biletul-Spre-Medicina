26b68559c0535d83740799b2732ba58b2275004c
biletulspremedicina <biletulspremedicina@proton.me>
2026-09-14 15:57:03 +0300

    Updated StudentDashboard.tsx

--- src/pages/StudentDashboard.tsx
+++ src/pages/StudentDashboard.tsx
@@ -82,7 +82,7 @@ const UMFCD_IMAGE_SRC = '/UMFCD.png'; // Imaginea pentru Examene UMFCD
 // Completează adresele dintre ghilimele pentru imaginile din meniul din stânga.
 // Până atunci rămân iconițele actuale (inclusiv sigla UMFCD).
 const NAV_IMAGE_SOURCES = {
-  home: '', // Aici pui sursa imaginii ACASA
+  home: '/Home.png', // Aici pui sursa imaginii ACASA
   all: '', // Aici pui sursa imaginii SIMULARI BIOLOGIE
   practice: '', // Aici pui sursa imaginii ANTRENAMENT PE CAPITOLE
   umfcd: '', // Aici pui sursa imaginii EXAMENE UMFCD
